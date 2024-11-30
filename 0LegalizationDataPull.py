cfed72726273ea69b6edcfa681c186a8fb5dce42
Wayne <wayne.blunden@gmail.com>
2024-11-29 23:10:32 -0500

    updating README.md

--- 0LegalizationDataPull.py
+++ 0LegalizationDataPull.py
@@ -1,10 +1,17 @@
+'''
+This segment of code will pull two tables from Wikipedia and merge them into a single table. The first table will list the marijuana legalization status of US 
+    territories. The second table is a list of minimum wage values for all 50 US states and Washington DC. The merged table will display all 50 states and Washington 
+    DC, those that have some form of marijuana legalization will have entries designating the Legalization Date and Sale Date (date first legal sales could be made). 
+    This table is output to a .csv file [Legalization.csv]. This bit of code also does some quick preliminary cleaning to make exported dataset easier to manage in the 
+    'Cleaning' stage. 
+'''
+
 import pandas as pd
 import wikipedia
 import os
 
 # Finding the current working directory and loading it into variable cwd
 cwd = os.getcwd()
-#print(cwd)
 
 # Using Wikipedia library to get the URLs for the specific pages and loading into designated 'url' variable
 urlLegality = wikipedia.page("Legality of cannabis by U.S. jurisdiction").url
@@ -13,17 +20,14 @@ urlStates = wikipedia.page("List_of_US_states_by_minimum_wage").url
 # Reading webpage table data into designated variables
 legalityTables = pd.read_html(urlLegality)
 statesTables = pd.read_html(urlStates)
-#print(statesTables)
 
 # Reading the wanted tables into designated variables
 legality = legalityTables[-9]
 states = statesTables[0]
-#print(states)
 
 # Creating designated variables and loading it with a dataframe created from the data in designated variables
 dfLegality = pd.DataFrame(legality)
 dfStates = pd.DataFrame(states)
-#print(dfLegality)
 
 # Dropping unwanted columns from both dataframes
 dfStates = dfStates[['State']]
@@ -37,21 +41,6 @@ dfLegality.loc[dfLegality.State == 'Washington (state)','State'] = 'Washington'
 
 # Joining two tables
 dfMerged = pd.merge(dfLegality, dfStates,how='right')
-#print(dfMerged)
-
-
 
 # Export dataframe of imported legalization data to .csv file
 dfMerged.to_csv(os.path.join(cwd,'Data\Raw', 'Legalization.csv'),index=False)
-#print(dfLegality)
-
-
-
-# Removing US territories from dfLegality
-#stateCombined = pd.concat([dfLegality['State'],dfStates['State']])
-#uniqueValues = stateCombined.unique()
-
-
-
-
-# Making Washington and Washington D.C. designations unique between tables. 
--- Data/Raw/DataDictionary.md
+++ Data/Raw/DataDictionary.md
@@ -1,6 +1,22 @@
 # **Data Dictionary**
 This document can be used to review the descriptions of the used columns from various datasets 
 
+### Columns Wanted in Dataset for Analysis
+ | Column Name | Column Description | How 
+ |---|---|---|
+ | Accident Record | ID of accident |  |
+ | Street | Name of the street the accident occurred on |  |
+ | City | Name of the city the accident occurred in |  |
+ | County | Name of the county the accident occurred in |  |
+ | State | Name of the state the accident occurred in |  |
+ | Zipcode | Zipcode where accident took place |  |
+ | Date | Date accident occured |  |
+ | Day/Night | Whether the accident occurred during the day or night | Derived from time of accident and recorded sun up sun down times for locality |
+ | ?Day of week? | Day of the week accident occured |  |
+ | LegalStatus | Whether marijuana use is legal in some form in state |  |
+ | LegalizationDate | Date of legalization within state |  |
+ | SalesDate | Date when sales were allowed to start |  |
+
 ### **US_Accidents_March23.csv**
 | Term | Description | Notes |
 | ---- | ----------- | --- |
